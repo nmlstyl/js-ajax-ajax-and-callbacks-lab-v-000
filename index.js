a2bfb0d2f1bd95d55ffa08d276949cfdb3b57fe4
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-07-07 22:19:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 $(document).ready(function (){
 });
+
+function searchRepositories(){
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      displaySearchResults(data)
+    }).fail(error => {
+      displayError()
+    })
+}
+
+function displaySearchResults(data){
+  debugger;
+  $('#results').html(
+    `<ul>` + data.items.map(d => {'<li>hi</li>'}) `<ul>`
+  )
+}
+
+function displayError(){
+
+}
