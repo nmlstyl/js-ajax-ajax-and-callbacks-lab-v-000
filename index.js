a38c5255ed6f05951249b4ebec8a74d0882c5903
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-07-08 02:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ function displayError(){
 function showCommits(el){
   $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repo}/commits/`, data => {
       displayCommits(data)
+    }).fail(error => {
+      displayError()
     })
 }
 
