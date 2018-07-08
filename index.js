554be4ed9037701314d945dd0c31273739c62a76
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-07-08 02:04:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function showSearchResults(data){
   $('#results').html(
     `<ul>` +  data.items.map(d => {return `<li>${d.name}<br><br>
                                                ${d.owner.login}<br><br>
-                                               <a href="#" data-repo="${d.name}" data-owner="${d.owner.login}" onClick="getCommits()"><br><br>
+                                               <a href="#" data-repo="${d.name}" data-owner="${d.owner.login}" onClick="getCommits()">Show Commits</a><br><br>
                                                </li>`}).join(' ') + `<ul>`
   )
 }
